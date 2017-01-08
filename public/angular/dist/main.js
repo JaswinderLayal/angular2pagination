34ec299433d9231186ee53958aa51789d42da610
Jaswinder singh <jas@jas.com>
2017-01-08 20:21:52 +0530

    Added Angular Pagination

---
+++ public/angular/dist/main.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * Created by Jaswinder on 8/9/2016.
+ */
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var main_component_ts_1 = require("./app/main.component.ts");
+platform_browser_dynamic_1.bootstrap(main_component_ts_1.MainComponent, []);
